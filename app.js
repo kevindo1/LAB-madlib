a8a0db873114294a21876733963db7d0ea1ca94a
Brett Seifried <seifriedbrett@gmail.com>
2021-08-27 14:40:56 -0700

    built template Para, started JS

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // import functions
+const adj1Input = document.getElementById("adj-1-input");
+const noun1Input = document.getElementById("noun-1-input");
+const verb1Input = document.getElementById("verb-1-input");
+const noun2Input = document.getElementById("noun-2-input");
+const verb2Input = document.getElementById("verb-2-input");
+const adj2Input = document.getElementById("adj-2-input");
+const adj3Input = document.getElementById("adj-3-input");
+const noun3Input = document.getElementById("noun-3-input");
+const noun4Input = document.getElementById("noun-4-input");
+const submitButton = document.getElementById("button");
 
 // reference needed DOM elements
 
-
-// set event listeners 
+// set event listeners
+submitButton.addEventListener('click', () => {
+    const adj1Output = document.getElementById('adj-1');
+    const noun1Output = document.getElementById('noun-1');
+    const verb1Output = document.getElementById('verb-1');
+    const noun2Output = document.getElementById('noun-2');
+    const verb2Output = document.getElementById('verb-2'); 
+}); 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
