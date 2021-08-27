2a8f179bf10e57fadd0f6202d604f2d884f5df2d
Kevin Do <kevind0501@gmail.com>
2021-08-27 15:35:45 -0700

    Added javascript to hide and show elements

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 // import functions
-const adj1Input = document.getElementById("adj-1-input");
-const noun1Input = document.getElementById("noun-1-input");
-const verb1Input = document.getElementById("verb-1-input");
-const noun2Input = document.getElementById("noun-2-input");
-const verb2Input = document.getElementById("verb-2-input");
-const adj2Input = document.getElementById("adj-2-input");
-const adj3Input = document.getElementById("adj-3-input");
-const noun3Input = document.getElementById("noun-3-input");
-const noun4Input = document.getElementById("noun-4-input");
-const submitButton = document.getElementById("button");
+const adj1Input = document.getElementById('adj-1-input');
+const noun1Input = document.getElementById('noun-1-input');
+const verb1Input = document.getElementById('verb-1-input');
+const noun2Input = document.getElementById('noun-2-input');
+const verb2Input = document.getElementById('verb-2-input');
+const adj2Input = document.getElementById('adj-2-input');
+const adj3Input = document.getElementById('adj-3-input');
+const noun3Input = document.getElementById('noun-3-input');
+const noun4Input = document.getElementById('noun-4-input');
+const submitButton = document.getElementById('button');
 
 // reference needed DOM elements
-    const adj1Output = document.getElementById('adj-1');
-    const noun1Output = document.getElementById('noun-1');
-    const verb1Output = document.getElementById('verb-1');
-    const noun2Output = document.getElementById('noun-2');
-    const verb2Output = document.getElementById('verb-2'); 
-    const adj2Output = document.getElementById('adj-2'); 
-    const adj3Output = document.getElementById('adj-3'); 
-    const noun3Output = document.getElementById('noun-3'); 
-    const noun4Output = document.getElementById('noun-4'); 
+const adj1Output = document.getElementById('adj-1');
+const noun1Output = document.getElementById('noun-1');
+const verb1Output = document.getElementById('verb-1');
+const noun2Output = document.getElementById('noun-2');
+const verb2Output = document.getElementById('verb-2'); 
+const adj2Output = document.getElementById('adj-2'); 
+const adj3Output = document.getElementById('adj-3'); 
+const noun3Output = document.getElementById('noun-3'); 
+const noun4Output = document.getElementById('noun-4');     
 
+const hidden = document.getElementById('hidden');
+const inputPage = document.getElementById('input-page');
 
     // set event listeners
 submitButton.addEventListener('click', () => {
@@ -53,18 +55,13 @@ submitButton.addEventListener('click', () => {
     const noun4Out = noun4Input.value;
     noun4Output.textContent = noun4Out;
 
-    
-    
-    
-    
-    
-    
-
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
-
-
 }); 
+
+// set event listeners
+submitButton.addEventListener('click', () => {
     
-
-
+    hidden.style.display = 'block'; 
+    inputPage.style.display = 'none';
+});
